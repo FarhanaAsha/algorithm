7795a13b95084334c6dead631b13824fd9eb818a
FarhanaAsha <ashahossain6728@gmail.com>
2025-11-25 23:36:47 +0600

    Add files via upload

---
+++ Bellman-Ford/warmholes.cpp
@@ -0,0 +1,64 @@
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=499
+//Warmholes problem in Onlinejudge
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Edge
+{
+    int u, v, w;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c;
+    cin >> c;
+    while (c--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<Edge> edges(m);
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> edges[i].u >> edges[i].v >> edges[i].w;
+        }
+
+
+        vector<int> dist(n, INT_MAX);
+        dist[0] = 0;
+
+        for (int i = 1; i <= n - 1; i++)
+        {
+            for (auto &e : edges)
+            {
+                if (dist[e.u] != INT_MAX && dist[e.u] + e.w < dist[e.v])
+                {
+                    dist[e.v] = dist[e.u] + e.w;
+                }
+            }
+        }
+
+        bool flag = false;
+        for (auto &e : edges)
+        {
+            if (dist[e.u] != INT_MAX && dist[e.u] + e.w < dist[e.v])
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag)
+            cout << "possible\n";
+        else
+            cout << "not possible\n";
+    }
+
+    return 0;
+}
+
